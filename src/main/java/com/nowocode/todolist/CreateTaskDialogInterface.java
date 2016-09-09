9eeef03aa6aafe5d136a10d9d45f0d2a1337db26
Reza Jafar <rezajafar7865@gmail.com>
2016-09-08 17:50:23 -0700

    Update CreateTaskDialogInterface.java

--- src/main/java/com/nowocode/todolist/CreateTaskDialogInterface.java
+++ src/main/java/com/nowocode/todolist/CreateTaskDialogInterface.java
@@ -1,7 +1,7 @@
 package com.nowocode.todolist;
 
 /**
- * Created by Lati on 18.04.2016.
+ * Created by Reza on 18.04.2016.
  */
 public interface CreateTaskDialogInterface {
     public void createTask(String s, int priority);
